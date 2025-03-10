3acbdc8dc6edcee0c593ecf39a0a91e72978e6ee
Y.Matsuda <ymatsuda.zck@gmail.com>
2025-03-10 14:04:03 +0900

    feat: remove old issues directory in golang script

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,15 +6,13 @@ INPUT_DRY_RUN="${INPUT_DRY_RUN:-false}"
 
 echo "Dry-run mode: ${INPUT_DRY_RUN}"
 
-rm -rf ./issues
-
 /app/issue-scouter
 
 git config --global --add safe.directory /github/workspace
 git config --global user.name "github-actions[bot]"
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 
-git add README.md ./issues
+git add --all
 git commit -m "Update issue list"
 
 if [ "${INPUT_DRY_RUN}" = "true" ]; then
